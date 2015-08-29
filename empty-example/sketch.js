546c0f7a76f5c7c6d141ff8f9a65d8ad61ff4e25
n-sayenko <nataliyasayenko@gmail.com>
2015-08-29 14:20:25 -0400

    hi

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,9 +1,12 @@
-var cowImage, musicNoteImage, backgroundImage;
+var cowImage, catImage, musicNoteImage, backgroundImage;
 var cow, musicNote, background;
 
 function setup() {
   createCanvas(800,800);
   cowImage = loadImage("assets/cow.png");
+  musicNoteImage = loadImage("assets/note.jpg");
+  catImage = loadImage("assets/cat.jpg")
+
   cow = createSprite(width/2, height/2);
   cow.addImage("normal", cowImage);
 
