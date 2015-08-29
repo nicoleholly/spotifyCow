9b54d53405d1c7366728ae6c40afb4fa5a4da974
n-sayenko <nataliyasayenko@gmail.com>
2015-08-29 16:24:06 -0400

    obstaclesgit pullgit pull

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,67 +1,60 @@
 
 var cowImage, musicNoteImage, catImage, backgroundImage;
 var cow, musicNote, cat, background;
-var GRAVITY = .002;
+var GRAVITY = .004;
+var obstacles;
+var boxes = true;
 
 
 function setup() {
-  createCanvas(1000,1000);
+  createCanvas(1000, 1000);
   musicNoteImage = loadImage("assets/note.jpg");
   cowImage = loadImage("assets/cow.png");
   catImage = loadImage("assets/cat.jpg");
 
-<<<<<<< HEAD
-  cow = createSprite(20, 20, 20, 20);
-  note = createSprite(100, 100, 20, 20);
-  cat = createSprite(200, 200, 20, 20);
-  cat.addImage("normal", catImage);
-  cat.scale = .25;
-  cow.scale = .25;
-  note.scale = .25;
-
-
-
-=======
   cow = createSprite(130, 130, 20, 20);
   note = createSprite(300, 300, 20);
   cat = createSprite(450, 450, 20, 20);
 
-  cow.velocity.x = .8;
-  cow.addImage("normal", cowImage);
+  cat.scale = .25;
+  note.scale = .25;
+  cow.scale = .25;
+
+  cow.velocity.x = 1.2;
   cow.rotateToDirection = true;
->>>>>>> 704ddee99cea980df4f4fcad9b374ec49384d825
+  cow.addImage("normal", cowImage);
+  cat.addImage("normal", catImage);
+  note.addImage("normal", musicNoteImage);
 
   camera.position.y = height/2;
+  obstacles = new Group();
+  if (frameCount%20 == 0){
+    for(i=0;i<10;i++){
+    var box = createSprite(random(0,width), random(0,height));
+    obstacles.add(box);
+  }
+  }
 }
 
 function draw() {
+  cow.velocity.y += GRAVITY;
 
-<<<<<<< HEAD
-=======
-	cow.velocity.y += GRAVITY;
+  if(keyWentDown("s")){
+    cow.velocity.y = -.3;
+  }
+  if(keyWentDown("d")){
+    cow.velocity.y = .3;
+  }
 
-	if(keyWentDown("s")){
-		cow.velocity.y = -.3;
-	}
-	if(keyWentDown("d")){
-		cow.velocity.y = .3;
-	}
+  camera.position.x = cow.position.x + width/16;
 
-	camera.position.x = cow.position.x + width/4;
+  background(0, 0, 0); 
+  camera.off();
+  camera.on();
+  cow.collide(obstacles);
 
- 	background(247, 134, 131); 
-	camera.off();
-//	image(backgroundImage, 0, 450-190);
-	camera.on();
-
->>>>>>> 704ddee99cea980df4f4fcad9b374ec49384d825
-	drawSprite(cow);
-	drawSprite(note);
-	drawSprite(cat);
-
+ drawSprites();
 
 }
 
-function mousePressed() {
-	note.addImage("normal", musicNoteImage);
-}
+
