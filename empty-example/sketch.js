f63ea46588d54977fd489ac714c6f95ca3b63470
Nicole <nhb228@nyu.edu>
2015-08-29 15:26:35 -0400

    GRAVITYYY

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,6 +1,8 @@
 var cowImage, musicNoteImage, catImage, backgroundImage;
 var direction = 90; 
 var cow, musicNote, cat, background;
+var GRAVITY = .003;
+
 
 function setup() {
   createCanvas(800,800);
@@ -8,21 +10,33 @@ function setup() {
   cowImage = loadImage("assets/cow.png");
   catImage = loadImage("assets/cat.jpg");
 
-  cow = createSprite(20, 20, 20, 20);
-  note = createSprite(100, 100, 20, 20);
-  cat = createSprite(200, 200, 20, 20);
-  console.log("here");
+  cow = createSprite(130, 130, 20, 20);
+  note = createSprite(300, 300, 20);
+  cat = createSprite(450, 450, 20, 20);
+
+  cow.velocity.x = 1;
 
 }
 
 function draw() {
-	background("#fae");
 	drawSprite(cow);
 	drawSprite(note);
 	drawSprite(cat);
+
+	cow.addImage("normal", cowImage);
+	cow.rotateToDirection = true;
+	cow.velocity.y += GRAVITY;
+
+	if(keyWentDown("s")){
+		cow.velocity.y = -.3;
+	}
+	if(keyWentDown("d")){
+		cow.velocity.y = .3;
+	}
+
+
 }
 function mousePressed() {
-	cow.addImage("normal", cowImage);
 	note.addImage("normal", musicNoteImage);
 	cat.addImage("normal", catImage);
 }
