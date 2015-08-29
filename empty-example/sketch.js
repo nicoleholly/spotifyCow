d40256b63220acfb5e9566b4f260a1a255fb697e
n-sayenko <nataliyasayenko@gmail.com>
2015-08-29 17:33:28 -0400

    catbreead

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -8,9 +8,9 @@ var obstacles;
 function setup() {
   createCanvas(1200, 800);
 
-  musicNoteImage = loadImage("assets/note.jpg");
-  cowImage = loadImage("assets/cow.png");
-  catImage = loadImage("assets/cat.jpg");
+  musicNoteImage = loadImage("assets/note.png");
+  cowImage = loadImage("assets/cow2.png");
+  catImage = loadImage("assets/cat2.gif");
 
   cow = createSprite(130, 130, 20, 20);
   note = createSprite(300, 300, 20);
@@ -83,7 +83,7 @@ function draw() {
   		cow.scale += .04;
   	}
 
-//	cow.collide(obstacles);
+  cow.collide(obstacles);
   background(0, 0, 0); 
   camera.off();
   camera.on();
