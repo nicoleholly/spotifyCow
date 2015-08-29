bd886e3be43e650e1c1ee674dee339ca86bf5be1
Nicole <nhb228@nyu.edu>
2015-08-29 18:44:55 -0400

    da earthh

--- cow-game/sketch.js
+++ cow-game/sketch.js
@@ -1,6 +1,6 @@
 
-var cowImage, musicNoteImage, catImage, backgroundImage;
-var cow, musicNote, background;
+var cowImage, musicNoteImage, catImage, backgroundImage, earthImage;
+var cow, musicNote, background, earth;
 var GRAVITY = .004;
 var obstacles;
 
@@ -14,9 +14,12 @@ function setup() {
   musicNoteImage = loadImage("assets/spotify.png");
   cowImage = loadImage("assets/cow2.png");
   catImage = loadImage("assets/cat2.gif");
+  earthImage = loadImage("assets/earth.gif");
 
+  earth = createSprite(500, 500,700, 700);
   cow = createSprite(130, 130, 20, 20);
   note = createSprite(300, 300, 20);
+  earth.scale = .5;
 
   cow.scale = .25;
 
@@ -26,6 +29,8 @@ function setup() {
 
   camera.position.y = height/2;
   
+  earth.addImage("normal", earthImage);
+
 }
 
 function draw() {
@@ -44,7 +49,7 @@ function draw() {
  	 camera.position.x = cow.position.x + width/16;
 
 
- 	cow.collide(obstacles);
+ 	//cow.collide(obstacles);
  	camera.position.x = cow.position.x + width/16;
  	if (frameCount%90 == 0){
   	for(i=0;i<3;i++){
@@ -70,11 +75,11 @@ function draw() {
 	}
 
 	if(cow.overlap(cats, collect)){
-    	cow.scale -= .008;
+    	cow.scale -= .08;
   	}
   
   if(cow.overlap(notes, collect)){
-  		cow.scale += .008;
+  		cow.scale += .08;
   }
 
   	camera.on();
