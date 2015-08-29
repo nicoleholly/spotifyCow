e86b538f4291a2c77b2795af01f0060bd625e1d2
Nicole <nhb228@nyu.edu>
2015-08-29 17:52:53 -0400

    this my commit

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -7,52 +7,47 @@ var obstacles;
 
 function setup() {
   createCanvas(1200, 800);
+  obstacles = new Group();
+  cats = new Group();
+  notes = new Group();
+  obstacles = new Group();
 
-  musicNoteImage = loadImage("assets/note.png");
+  musicNoteImage = loadImage("assets/spotify.png");
   cowImage = loadImage("assets/cow2.png");
   catImage = loadImage("assets/cat2.gif");
 
   cow = createSprite(130, 130, 20, 20);
   note = createSprite(300, 300, 20);
 
-  note.scale = .25;
   cow.scale = .25;
 
   cow.velocity.x = 1.2;
   cow.rotateToDirection = true;
   cow.addImage("normal", cowImage);
-  note.addImage("normal", musicNoteImage);
 
   camera.position.y = height/2;
-
-  
-  obstacles = new Group();
-  cats = new Group();
-  notes = new Group();
   
 }
 
 function draw() {
-
-  cow.velocity.y += GRAVITY;
-
-  if(keyWentDown("s")){
-    cow.velocity.y = -.3;
-  }
-  if(keyWentDown("d")){
-    cow.velocity.y = .3;
-  }
-  cow.velocity.y += GRAVITY;
-
-  camera.position.x = cow.position.x + width/16;
-
-  background(0, 0, 0); 
-  obstacles = new Group();
+	background(0, 0, 0); 
+  	cow.velocity.y += GRAVITY;
 
 
-  cow.collide(obstacles);
-  camera.position.x = cow.position.x + width/16;
-  if (frameCount%90 == 0){
+  	if(keyWentDown("s")){
+    	cow.velocity.y = -.3;
+  	}
+  	if(keyWentDown("d")){
+    	cow.velocity.y = .3;
+  	}
+  	cow.velocity.y += GRAVITY;
+
+ 	 camera.position.x = cow.position.x + width/16;
+
+
+ 	cow.collide(obstacles);
+ 	camera.position.x = cow.position.x + width/16;
+ 	if (frameCount%90 == 0){
   	for(i=0;i<3;i++){
   		var box = createSprite(cow.position.x + width, random(0 + (height*.15), height), random(0,40), random(0,40));
     	obstacles.add(box);
@@ -70,7 +65,7 @@ function draw() {
 	if(frameCount%150 == 0){
 		var newNote = createSprite(cow.position.x + width, random(0+(height*.15), height), 20, 20);
 		newNote.addImage("normal", musicNoteImage);
-		newNote.scale = .25;
+		newNote.scale = .15;
 		newNote.velocity.x = -.75;
 		notes.add(newNote);
 	}
@@ -84,12 +79,10 @@ function draw() {
   	}
 
 //  cow.collide(obstacles);
-  background(0, 0, 0); 
-  camera.off();
-  camera.on();
-  camera.position.x = cow.position.x + width/16;
-  drawSprites();
-
+  	camera.off();
+  	camera.on();
+  	camera.position.x = cow.position.x + width/16;
+  	drawSprites();
 }
 
 function collect(collector, collected){
